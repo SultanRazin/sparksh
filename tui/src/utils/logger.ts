77b420d558ce3cb8c072e8481d96bb20d9a9a85c
SultanRazin <92024243+SultanF1@users.noreply.github.com>
2026-01-31 11:28:51 +0300

    rename log file

--- tui/src/utils/logger.ts
+++ tui/src/utils/logger.ts
@@ -2,7 +2,7 @@ import { appendFileSync } from "fs";
 
 const log = (...args: unknown[]) =>
   appendFileSync(
-    "debug.log",
+    ".sparksh.log",
     args
       .map((a) => (typeof a === "object" ? JSON.stringify(a) : String(a)))
       .join(" ") + "\n",
